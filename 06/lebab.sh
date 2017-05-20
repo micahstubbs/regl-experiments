80120f53e4428cf5b4c82b7ff58ea075a8728d39
Micah Stubbs <micah.stubbs@gmail.com>
2017-05-20 00:54:03 -0700

    add iteration with magma and reversed color domain

---
+++ 06/lebab.sh
@@ -0,0 +1,25 @@
+# safe
+lebab --replace vis.js --transform arrow
+lebab --replace vis.js --transform for-of
+lebab --replace vis.js --transform for-each
+lebab --replace vis.js --transform arg-rest
+lebab --replace vis.js --transform arg-spread
+lebab --replace vis.js --transform obj-method
+lebab --replace vis.js --transform obj-shorthand
+lebab --replace vis.js --transform multi-var
+# unsafe
+lebab --replace vis.js --transform let
+lebab --replace vis.js --transform template
+
+# safe
+lebab --replace layouts.js --transform arrow
+lebab --replace layouts.js --transform for-of
+lebab --replace layouts.js --transform for-each
+lebab --replace layouts.js --transform arg-rest
+lebab --replace layouts.js --transform arg-spread
+lebab --replace layouts.js --transform obj-method
+lebab --replace layouts.js --transform obj-shorthand
+lebab --replace layouts.js --transform multi-var
+# unsafe
+lebab --replace layouts.js --transform let
+lebab --replace layouts.js --transform template
